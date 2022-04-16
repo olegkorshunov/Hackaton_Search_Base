8abdd16fd75563c6bd5591e31fbdab19f58ee843
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2022-04-16 09:27:11 +0300

    1st checkpoint

--- search/search_solution.py
+++ search/search_solution.py
@@ -1,20 +1,69 @@
-import numpy as np 
-from .search import Base
+import os
+import pickle
 from typing import List, Tuple
 
-class SearchSolution(Base):
+import faiss
+import gdown
+import numpy as np
 
-    def __init__(self, data_file='./data/train_data.pickle', 
-                 data_url='https://drive.google.com/uc?id=1D_jPx7uIaCJiPb3pkxcrkbeFcEogdg2R') -> None:
+from .search import Base
+
+
+class SearchSolution(Base):
+    def __init__(
+        self,
+        data_file="./data/train_data.pickle",
+        data_url="https://drive.google.com/uc?id=1D_jPx7uIaCJiPb3pkxcrkbeFcEogdg2R",
+        top_k=4,
+        dim=512,
+    ) -> None:
         self.data_file = data_file
         self.data_url = data_url
-        pass
+        self.top_k = top_k  # nearest neighbors
+        self.dim = dim
+        self.index = faiss.IndexFlatL2(self.dim)  # build the index
+
+    def add_vectors2index(self, vectors: np.array) -> None:
+        self.index.add(vectors)
 
     def set_base_from_pickle(self):
-        pass
+        """
+        Downloads the data, if it does not exist.
+        Sets reg_matrix and pass_dict
+
+        reg_matrix : np.array(N, 512)
+        pass_dict : dict -> dict[idx] = [np.array[1, 512]]
+        """
+        if not os.path.isfile(self.data_file):
+            if not os.path.isdir("./data"):
+                os.mkdir("./data")
+            gdown.download(self.data_url, self.data_file, quiet=False)
+
+        with open(self.data_file, "rb") as f:
+            data = pickle.load(f)
+
+        self.reg_matrix = [None] * len(data["reg"])
+        self.ids = {}
+        for i, key in enumerate(data["reg"]):
+            self.reg_matrix[i] = data["reg"][key][0][None]
+            self.ids[i] = key
+
+        self.reg_matrix = np.concatenate(self.reg_matrix, axis=0).astype("float32")
+        self.add_vectors2index(self.reg_matrix)
+        self.pass_dict = data["pass"]
 
     def search(self, query: np.array) -> List[Tuple]:
-        pass
+        if query.ndim == 1:
+            query = np.expand_dims(query, axis=0)
+        D, I = self.index.search(query.astype("float32"), self.top_k)
+        return list(zip(I[0], D[0]))
+
+    def insert(self, feature: np.array) -> None:
+        if feature.ndim == 1:
+            feature = np.expand_dims(feature, axis=0)
+        feature = feature.astype("float32")
+        self.add_vectors2index(feature)
+        self.reg_matrix = np.concatenate((self.reg_matrix, feature), axis=0)
 
     def cos_sim(self, query: np.array) -> np.array:
         pass
