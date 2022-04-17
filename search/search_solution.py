577411681436ff785263e2ff8b0f327527fa0588
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2022-04-17 13:54:28 +0300

    finaaaal commit

--- search/search_solution.py
+++ search/search_solution.py
@@ -15,7 +15,7 @@ class SearchSolution(Base):
         data_file="./data/train_data.pickle",
         data_url="https://drive.google.com/uc?id=1D_jPx7uIaCJiPb3pkxcrkbeFcEogdg2R",
         nlist=2048,  # the number of cells
-        nprobe=128,  # the number of cells (out of nlist) that are visited to perform a search
+        nprobe=256,  # the number of cells (out of nlist) that are visited to perform a search
         top_k=8,  # nearest neighbors
         dim=512,
     ) -> None:
